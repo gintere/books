49edf1329f74e23ba44aa93c7c106cb4bd5a8ddd
Diana Gintere <diana.gintere@gmail.com>
2021-12-18 11:07:08 +0200

    application test

---
+++ src/test/java/lv/sda/books/ApplicationTest.java
@@ -0,0 +1,13 @@
+package lv.sda.books;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ApplicationTest {
+
+    @Test
+    public void sampleTest() {
+        Assertions.assertEquals(1, 1);
+    }
+
+}
